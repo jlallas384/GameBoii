c864fc1cbdbbe09c65d9b898612c949d97b332f0
jlallas384 <jlallas384@gmail.com>
2024-08-08 22:48:34 +0800

    fix color white code

--- src/ppu/lcd.h
+++ src/ppu/lcd.h
@@ -7,7 +7,7 @@ using Color = uint16_t;
 class LCD {
 public:
     constexpr static uint8_t height = 144, width = 160;
-    constexpr static Color white = 0;
+    constexpr static Color white = 0x7fff;
     virtual ~LCD() = default;
     virtual void refresh() = 0;
     virtual void setPixel(uint8_t i, uint8_t j, Color color) = 0;
